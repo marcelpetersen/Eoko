cb1372814ea798928de167111072c0df5c463c89
Immunity-Project <gkm24@drexel.edu>
2017-02-22 23:07:44 -0500

    messaging is done! (two way messages at least...)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,14 @@
 angular.module('app.controllers', [])
 
-  .controller('profileCtrl', ['$scope', '$stateParams', 'UserInfo', 'OtherInfo', '$firebaseObject', '$ionicTabsDelegate', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+  .controller('profileCtrl', ['$scope', '$stateParams', 'UserInfo', 'OtherInfo', '$firebaseObject', '$ionicTabsDelegate','$timeout', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-    function ($scope, $stateParams, UserInfo, OtherInfo, $firebaseObject, $ionicTabsDelegate) {
+    function ($scope, $stateParams, UserInfo, OtherInfo, $firebaseObject, $ionicTabsDelegate,$timeout) {
 
-      $scope.user = "";
-      var usr;
+      var usr = UserInfo.getUserInfo();
       $scope.$on('$ionicView.beforeEnter', function () //before anything runs
       {
-        console.log($stateParams.avatarClicked);
+         $scope.user = UserInfo.getUserInfo();
         if ($stateParams.avatarClicked == 'true') {
           console.log("other");
           $ionicTabsDelegate.showBar(false);
@@ -17,7 +16,8 @@ angular.module('app.controllers', [])
           console.log($scope.user);
         }
         else {
-          if (usr == undefined) {
+          if (usr == undefined || usr.email=="") {
+            console.log("undefined usr");
             firebase.auth().onAuthStateChanged(function (user) {
               usr = firebase.auth().currentUser;
 
@@ -42,6 +42,8 @@ angular.module('app.controllers', [])
             });
           }
           else {
+            console.log("userinfo is:", UserInfo.getUserInfo());
+            $scope.user = UserInfo.getUserInfo();
             if ($scope.user.notifications) {
               $scope.friendR = Object.keys($scope.user.notifications).length;
             }
@@ -50,7 +52,7 @@ angular.module('app.controllers', [])
             }
           }
         }
-
+        $timeout(function () {$scope.$apply();});
       });
 
 
@@ -73,8 +75,10 @@ angular.module('app.controllers', [])
 
       $scope.$on('$ionicView.beforeEnter', function () //before anything runs
       {
-        console.log("working?");
-        if ($scope.notifications.length == 0 && authUser == undefined) {
+        usr = UserInfo.getUserInfo();
+        console.log("working?",UserInfo.getUserInfo());
+        if ($scope.notifications.length == 0 && authUser == undefined || usr.email=="")
+         {
           console.log("Why yes it is!");
           firebase.auth().onAuthStateChanged(function (user) {
             authUser = firebase.auth().currentUser;
@@ -92,7 +96,8 @@ angular.module('app.controllers', [])
               });
           });
         }
-        else {
+        else
+         {
           refActivate();
         }
 
@@ -162,9 +167,7 @@ angular.module('app.controllers', [])
             req.child(selectedlist[i]).once('value').then(function (snap) {
               $scope.goingList.push({info: snap.val()});
               console.log("record: ", snap.val());
-              $timeout(function () {
-                $scope.$apply();
-              });
+              $timeout(function () {$scope.$apply();});
             });
 
           }
@@ -304,6 +307,8 @@ angular.module('app.controllers', [])
         };
 
       })();
+
+
 
       var weekday = new Array();
       weekday[0] = "Sunday";
@@ -563,12 +568,6 @@ angular.module('app.controllers', [])
           console.log($scope.owning);
 
 
-$scope.onHold = function(clicked)
-{
-
-};
-
-
         }
       });
 
@@ -635,16 +634,39 @@ $scope.createMessage = function()
     var uniqueMessageID = combino[0].eyed + '_' + combino[1].eyed;
     console.log(uniqueMessageID);
 
-    var res = firebase.database().ref("Buildings").child(usor.displayName + "/Chats/" + uniqueMessageID);
+    var res = firebase.database().ref("Buildings").child(usor.displayName + "/Chats");
 
-    res.set({
-        'chatTitle': "",
-        'chatIDs': [usor.uid,messageUser.$id]
-    }).then(function()
-    {
-        $scope.closePopover();
-        $state.go('chatTab');
+    res.child(uniqueMessageID).once('value', function(snapshot) {
+        if(snapshot.val() !== null)
+        {
+            $scope.closePopover();
+            $state.go('chatPage', {
+              'otherID': messageUser.$id,
+              'convoID': uniqueMessageID
+            });
+        }
+        else
+        {
+            res.child(uniqueMessageID).set({
+            'chatTitle': "",
+            'chatIDs': [usor.uid,messageUser.$id]
+            }).then(function()
+            {
+                $scope.closePopover();
+                $state.go('chatPage', {
+                  'otherID': messageUser.$id,
+                  'convoID': uniqueMessageID
+                });
+            });
+        }
     });
+
+
+        
+
+    
+    
+    
     
 };
 
@@ -1330,10 +1352,10 @@ $scope.openPopover = function($event,notify) {
 
 
 
-.controller('chatTabCtrl', ['$scope', '$stateParams','$firebaseObject','UserInfo','$firebaseArray', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('chatTabCtrl', ['$scope', '$stateParams','$firebaseObject','UserInfo','$firebaseArray','$timeout', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams,$firebaseObject,UserInfo,$firebaseArray) {
+function ($scope, $stateParams,$firebaseObject,UserInfo,$firebaseArray,$timeout) {
 
 var usr = UserInfo.getUserInfo();
 var authUser = firebase.auth().currentUser;
@@ -1387,46 +1409,124 @@ $scope.$on('$ionicView.beforeEnter', function() //before anything runs
                     console.log("Error:", error);
                   });
            }
-    });
     
+    });
+
+
+
             function getInfo(x)
             {
                 var rec = firebase.database().ref("Buildings").child(authUser.displayName + "/Users");
-                rec.once('value').then(function(snap)
-                {
+                rec.once('value').then(function(snap){
                     for(var i = 0; i < $scope.conversations.length; i++)
                     {
+                        //console.log();
                         if (x[i].chatIDs.indexOf(authUser.uid) > -1)   //one of my convos
                         {
+
                             if(x[i].chatTitle == "")   //two way talk
                             {
-                                //console.log($scope.conversations[i]);
-                                var partner = (x[i].chatIDs.indexOf(authUser.uid) == 0) ? x[i].chatIDs[1] : x[i].chatIDs[0];
+                                console.log("innerfor");
+                                var lastmessage = "";
+                                var lasttime = "";
+                                for(var j in x[i].messages)
+                                {
+                                    lastmessage = x[i].messages[j].text;
+                                    lasttime = x[i].messages[j].time;
+                                }
+
+                                partner = (x[i].chatIDs.indexOf(authUser.uid) == 0) ? x[i].chatIDs[1] : x[i].chatIDs[0];
+
                                 $scope.conversations[i].avatar = snap.val()[partner].avatar;
                                 $scope.conversations[i].name = snap.val()[partner].name;
-                                //$scope.conversations[i].messages = snap.val()[partner].name;
+                                $scope.conversations[i].partnerID = partner;
+                                $scope.conversations[i].chatID = x[i].$id;
+                                $scope.conversations[i].lastmessage = lastmessage;
+                                $scope.conversations[i].lasttime = lasttime;
                             }
                         }
                     }
-                });
-                
+                    $timeout(function () {$scope.$apply();});
+                }); 
             }
 
 }])
 
 
 
-.controller('chatPageCtrl', ['$scope', '$stateParams','$firebaseObject','UserInfo','$firebaseArray', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('chatPageCtrl', ['$scope', '$stateParams','$firebaseObject','UserInfo','$firebaseArray','$ionicScrollDelegate', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams,$firebaseObject,UserInfo,$firebaseArray) {
+function ($scope, $stateParams,$firebaseObject,UserInfo,$firebaseArray,$ionicScrollDelegate) {
+
+    var authUser = firebase.auth().currentUser;
+    $scope.myId = authUser.uid;
+    var ref = firebase.database().ref("Buildings").child(authUser.displayName);
+    var partnerID = $stateParams.otherID;
+    var convoID = $stateParams.convoID;
+    var currentnum = 0;
+
+    $scope.$on('$ionicView.beforeEnter', function() //before anything runs
+    {
+        ref.child('Chats').child(convoID).once("value").then(function(snap)
+        {
+            console.log("the whole chat", snap.val());
+            $scope.chatObj = snap.val();
+        });
+
+        ref.child('Users').child(partnerID).once("value").then(function(snap)
+        {
+            console.log("the partner", snap.val());
+            $scope.partner = snap.val();
+        });
+
+    });
+
+
+    $scope.$on('$ionicView.afterEnter', function() //before anything runs
+    {
+        console.log("ref is:" , ref);
+        $ionicScrollDelegate.scrollBottom();
+        $scope.messages = $firebaseArray(ref.child('Chats').child(convoID + '/messages'));
+        $scope.messages.$loaded()
+          .then(function(x) {
+            console.log("messages are loaded", x)
+            currentnum =  Object.keys($scope.messages).length;
+          })
+          .catch(function(error) {
+            console.log("Error:", error);
+          });
+      });
+
+
+$scope.data ={messageText : ""};
 
 
 
-      $scope.newConversation = function () {
 
+       $scope.sendMessage = function() {
+            console.log("starting messagesend");
+            var d = new Date();
+            d = d.toLocaleTimeString().replace(/:\d+ /, ' ');
+            console.log("message:", $scope.data.messageText );
+            console.log("toadded: ", $scope.messages);
 
-      };
+                 $scope.messages.$add({
+                  userId: authUser.uid,
+                  text: $scope.data.messageText,
+                  time: d
+                 
+                });
+            $scope.data.messageText  = "";
+            $ionicScrollDelegate.scrollBottom();
+            
+           
+        };
+
+       $scope.closeKeyboard = function()
+       {
+
+       };
 
 
 
